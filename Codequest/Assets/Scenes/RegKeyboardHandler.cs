48786be724ca8504ff67723794262652996758af
Katrina <katrinamicaellabarbosa@gmail.com>
2025-06-27 03:40:19 +0800

    rect transform to input fields when typing

---
+++ Codequest/Assets/Scenes/RegKeyboardHandler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class RegKeyboardHandler : MonoBehaviour
+{
+    public RectTransform panelToMove;
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = panelToMove.localPosition;
+    }
+
+    void Update()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        if (TouchScreenKeyboard.visible)
+        {
+            // Move the panel up when keyboard is visible
+            panelToMove.localPosition = Vector3.Lerp(panelToMove.localPosition, new Vector3(originalPosition.x, 400f, originalPosition.z), Time.deltaTime * 10f);
+        }
+        else
+        {
+            // Reset position when keyboard is hidden
+            panelToMove.localPosition = originalPosition;
+        }
+#endif
+    }
+}
